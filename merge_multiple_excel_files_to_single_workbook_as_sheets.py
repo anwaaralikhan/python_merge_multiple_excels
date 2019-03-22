c7a6ad4ab2b5f7f8019566591f0e71ae121215c3
Digital Alchemist <38991296+anwaaralikhan@users.noreply.github.com>
2019-03-22 14:55:56 -0400

    Update merge_multiple_excel_files_to_single_workbook_as_sheets.py

--- merge_multiple_excel_files_to_single_workbook_as_sheets.py
+++ merge_multiple_excel_files_to_single_workbook_as_sheets.py
@@ -1,18 +1,28 @@
-
 import xlsxwriter
 import os
 import csv
 
 
+#################################################################
+#################################################################
 directory_location ='C:\folder_with_multiple_excel_files'
 merge_file_location ='C:\output\merged_excel_workbook.xlsx'
 
+
+#################################################################
+#################################################################
+#load all files
 files_in_directory = os.listdir(directory_location)
 
+
+#################################################################
+#################################################################
 # create merge workbook
 workbook = xlsxwriter.Workbook(merge_file_location)
 
 
+#################################################################
+#################################################################
 for file in files_in_directory:
     fName = directory_location  + '\\' + file
     basic_name = file.split('_')  # this part is optional, my files were _ separated with time_stamp ex: file1_03222019
@@ -27,5 +37,9 @@ for file in files_in_directory:
                 worksheet.write(row_count,col_count,col)
                 col_count +=1
             row_count +=1
+
+            
+#################################################################
+#################################################################
 
 workbook.close()
